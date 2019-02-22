15e7cf4c5f35e2bc49170727e915e49f4ebf38d2
londonappbrewery <londonappbrewery@gmail.com>
2019-02-22 11:41:13 +0000

    updating TODOs

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -5,28 +5,27 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    // TODO: Uncomment to create question bank
+    // Create question bank using the Question class for each item in the array
+//    private Question[] mQuestionBank = new Question[] {
+//            new Question(R.string.question_1, true),
+//            new Question(R.string.question_2, true),
+//            new Question(R.string.question_3, true),
+//            new Question(R.string.question_4, true),
+//            new Question(R.string.question_5, true),
+//            new Question(R.string.question_6, false),
+//            new Question(R.string.question_7, true),
+//            new Question(R.string.question_8, false),
+//            new Question(R.string.question_9, true),
+//            new Question(R.string.question_10, true),
+//            new Question(R.string.question_11, false),
+//            new Question(R.string.question_12, false),
+//    };
+
     // TODO: Declare constants here
 
 
     // TODO: Declare member variables here:
-
-
-    // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
